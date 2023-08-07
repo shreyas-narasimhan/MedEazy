055aaba45e0f54bbbaeab4d9133d248d2c1b73dd
shreyas-narasimhan <66430076+shreyas-narasimhan@users.noreply.github.com>
2023-08-07 22:14:12 +0530

    Add files via upload

---
+++ data/arrays.js
@@ -0,0 +1,114 @@
+export const states = [
+  {
+    id: 0,
+    state: "Chennai",
+    lgas: [
+      "Chennai",
+    ],
+  },
+  {
+    id: 1,
+    state: "Salem",
+    lgas: [
+      "Salem",
+    ],
+  },
+  {
+    id: 2,
+    state: "Madurai",
+    lgas: [
+      "Madurai",
+    ],
+  },
+  {
+    id: 3,
+    state: "Erode",
+    lgas: [
+      "Erode",
+    ],
+  },
+  {
+    id: 4,
+    state: "Coimbatore",
+    lgas: [
+      "Coimbatore",
+    ],
+  },
+  {
+    id: 5,
+    state: "Tiruchirapalli",
+    lgas: [
+      "Tiruchirapalli",
+    ],
+  },
+  {
+    id: 6,
+    state: "Kumbakonam",
+    lgas: [
+      "Kumbakonam",
+    ],
+  },
+  {
+    id: 7,
+    state: "Krishnagiri",
+    lgas: [
+      "Krishnagiri",
+    ],
+  },
+  {
+    id: 8,
+    state: "Ramanathapuram",
+    lgas: [
+      "Ramanathapuram",
+    ],
+  },
+  {
+    id: 9,
+    state: "Theni",
+    lgas: [
+      "Theni",
+    ],
+  },
+  {
+    id: 10,
+    state: "Dharmapuri",
+    lgas: [
+      "Dharmapuri",
+    ],
+  },
+  {
+    id: 11,
+    state: "Kanchipuram",
+    lgas: [
+      "Kanchipuram",
+    ],
+  },
+  {
+    id: 12,
+    state: "Thanjavur",
+    lgas: [
+      "Thanjavur",
+    ],
+  },
+  {
+    id: 13,
+    state: "Cuddalore",
+    lgas: [
+      "Cuddalore",
+    ],
+  },
+  {
+    id: 14,
+    state: "Vellore",
+    lgas: [
+      "Vellore",
+    ],
+  },
+  {
+    id: 15,
+    state: "Tirunelveli",
+    lgas: [
+      "Tirunelveli",
+    ],
+  },
+];
